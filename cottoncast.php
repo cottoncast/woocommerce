bdeb1f6b6b95deefc896a0606daf9951c3af1fb7
Chris Luttikhuis <chris@nerdbynature.nl>
2021-03-05 15:51:51 +0100

    REL v1.4.0

--- cottoncast.php
+++ cottoncast.php
@@ -2,17 +2,17 @@
 /**
 * Plugin Name: Cottoncast Products
 * Plugin URI: https://cottoncast.com/integrations
-* Description: Cottoncast. Thank you for joining us on our journey to become the best, not the biggest, Print on Demand company.
-* Version: 1.3.1
+* Description: Cottoncast. Dropshipping fashion for your webshop.
+* Version: 1.4.0
 * Author: Cottoncast
 * Author URI: https://cottoncast.com/
 * Developer: Chris Luttikhuis
 * Developer URI: https://cottoncast.com/
 *
-* WC requires at least: 2.2
-* WC tested up to: 3.8
+* WC requires at least: 3.8
+* WC tested up to: 5.0
 *
-* Copyright: © 2019 Cottoncast.
+* Copyright: © 2021 Cottoncast.
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -20,7 +20,7 @@ exit; // Exit if accessed directly
 }
 
 define('COTTONCAST_PLUGIN_PATH', __FILE__);
-define('COTTONCAST_PLUGIN_VERSION', '1.3.1');
+define('COTTONCAST_PLUGIN_VERSION', '1.4.0');
 
 	/**
 	 * Check if WooCommerce is active
@@ -30,9 +30,7 @@ define('COTTONCAST_PLUGIN_VERSION', '1.3.1');
 		// Installation and configuration
 		include( 'installation.php' );
 
-		// Category mapping
-		include( 'category/cronjob.php' );
-
+		//Category mapping
 		include( 'category/condition.php' );
 		include( 'category/category.php' );
 		include( 'category/agegroup.php' );
